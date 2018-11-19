5480bbb9918f0c6bb483f66eeebb39fe3edf02c6
Gianluca Orpello <g.orpello@gmail.com>
2018-11-19 15:44:39 +0100

    Recover The missing file... Check TVListViewController

---
+++ Viewer iOs/Controllers/Poster/PosterViewController.swift
@@ -0,0 +1,103 @@
+//
+//  PosterViewController.swift
+//  Viewer
+//
+//  Created by Gianluca Orpello on 14/11/2018.
+//  Copyright © 2018 Gianluca Orpello. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PosterViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary;
+        imagePicker.allowsEditing = false
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    @IBOutlet weak var imageViewController: UIImageView!{
+        didSet{
+            imageViewController.layer.borderWidth = 2
+            imageViewController.layer.borderColor = UIColor.blue.cgColor
+            imageViewController.layer.cornerRadius = 20
+            imageViewController.clipsToBounds = true
+        }
+    }
+    
+    @IBAction func getImageFromGallery(_ sender: UIButton) {
+        
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus {
+        case .authorized:
+            
+            if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+                debugPrint("Button capture")
+                
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({
+                (newStatus) in
+                debugPrint("status is \(newStatus)")
+                if newStatus ==  PHAuthorizationStatus.authorized {
+                    
+                    if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+                        debugPrint("Button capture")
+                        self.present(self.imagePicker, animated: true, completion: nil)
+                    }
+                    debugPrint("success")
+                }
+            })
+            debugPrint("It is not determined until now")
+        default:
+            break
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage{
+            imageViewController.image = pickedImage
+        }
+        self.imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        switch segue.identifier {
+        case "postGlobalMessageSegue":
+            break
+        case "postImageSegue":
+            
+            guard imageViewController.image != nil else {
+                
+                let alert = UIAlertController(title: "Select Image", message: "You have to select an image to share.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                present(alert, animated: true, completion: nil)
+                
+                return
+            }
+            
+            if segue.identifier == "ChooseLocationSegue"{
+                if let destination = segue.destination as? TvListViewController{
+                    destination.image = imageViewController.image!
+                }
+            }
+            
+        default:
+            break
+        }
+    }
+    
+}
---
+++ Viewer/Controller/GlassOfficeViewController.swift
@@ -0,0 +1,70 @@
+//
+//  GlassOfficeViewController.swift
+//  Viewer
+//
+//  Created by Gianluca Orpello on 16/11/2018.
+//  Copyright © 2018 Gianluca Orpello. All rights reserved.
+//
+
+import UIKit
+
+class GlassOfficeViewController: UIViewController {
+
+    // MARK: - API Apple Developer Academy
+    var APIkey = "d88efc5ef767c002befbec5a9083e562"
+    var key: String{
+        return "?api_key=\(APIkey)"
+    }
+    var endpoint = "https://developer-academy.booqable.com/api/1/"
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let url = URL(string: endpoint+"orders"+key)
+        var request = URLRequest(url:url!)
+        request.httpMethod = "GET"
+        
+        getJson(from: url!)
+        
+    }
+    
+    //this function is fetching the json from URL
+    func getJson(from url: URL){
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                
+                //printing the json in console
+                print("Json: ")
+                print(jsonObj)
+                
+//                //getting the avengers tag array from json and converting it to NSArray
+//                if let heroeArray = jsonObj!.value(forKey: "avengers") as? NSArray {
+//                    //looping through all the elements
+//                    for heroe in heroeArray{
+//
+//                        //converting the element to a dictionary
+//                        if let heroeDict = heroe as? NSDictionary {
+//
+//                            //getting the name from the dictionary
+//                            if let name = heroeDict.value(forKey: "name") {
+//
+//                                //adding the name to the array
+//                                self.nameArray.append((name as? String)!)
+//                            }
+//
+//                        }
+//                    }
+//                }
+//
+//                OperationQueue.main.addOperation({
+//                    //calling another function after fetching the json
+//                    //it will show the names to label
+//                    self.showNames()
+//                })
+            }
+        }).resume()
+    }
+}
