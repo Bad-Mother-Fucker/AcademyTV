56575d8bdf37984dc22ce0bb5c3f898b4b38f7a9
Gianluca Orpello <g.orpello@gmail.com>
2018-11-27 13:14:08 +0100

    Working hard...

--- Viewer iOs/Controllers/Poster/PosterViewController.swift
+++ Viewer iOs/Controllers/Poster/PosterViewController.swift
@@ -1,50 +1,37 @@
 //
 //  PosterViewController.swift
-//  Viewer
+//  Viewer iOs
 //
-//  Created by Gianluca Orpello on 14/11/2018.
+//  Created by Gianluca Orpello on 27/11/2018.
 //  Copyright © 2018 Gianluca Orpello. All rights reserved.
 //
 
 import UIKit
 import Photos
 
-class PosterViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    
+class PosterViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
     var imagePicker = UIImagePickerController()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary;
-        imagePicker.allowsEditing = false
-        
-        tableView.tableFooterView = UIView()
-        
-        let userDefault = UserDefaults.standard
-        userDefault.addSuite(named: "com.Rogue.Viewer.ShareExt")
-        
-        if let dict = userDefault.value(forKey: "keynote") as? [String:Any]{
-            
-            let data = dict["imgData"] as! [Data]
-            let str = dict["name"] as! String
-            print(data,str)
-            userDefault.removeObject(forKey: "img")
-            userDefault.synchronize()
-            
-        }
-        
-    }
     
     @IBOutlet weak var imageViewController: UIImageView!{
         didSet{
+            imageViewController.contentMode = .scaleAspectFit
             imageViewController.layer.borderWidth = 2
             imageViewController.layer.borderColor = UIColor.blue.cgColor
-            imageViewController.layer.cornerRadius = 20
+            imageViewController.layer.cornerRadius = 10
             imageViewController.clipsToBounds = true
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary;
+        imagePicker.allowsEditing = false
+        
+    }
+
     @IBAction func getImageFromGallery(_ sender: UIButton) {
         
         let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
@@ -75,7 +62,7 @@ class PosterViewController: UITableViewController, UINavigationControllerDelegat
             break
         }
     }
-    
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage{
             imageViewController.image = pickedImage
@@ -88,7 +75,6 @@ class PosterViewController: UITableViewController, UINavigationControllerDelegat
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         switch segue.identifier {
         case "postGlobalMessageSegue":
             break
@@ -113,5 +99,4 @@ class PosterViewController: UITableViewController, UINavigationControllerDelegat
             break
         }
     }
-    
 }
